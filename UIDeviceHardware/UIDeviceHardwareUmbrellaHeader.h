2cca58a68953defe09e91a866e4a67243b1c5dcd
dgoon <kslee@hpcnt.com>
2015-06-01 15:31:56 +0900

    프레임웍 빌드용 프로젝트 생성

---
+++ UIDeviceHardware/UIDeviceHardwareUmbrellaHeader.h
@@ -0,0 +1,20 @@
+//
+//  UIDeviceHardware.h
+//  UIDeviceHardware
+//
+//  Created by dgoon on 2015. 6. 1..
+//  Copyright (c) 2015년 Hyperconnect. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for UIDeviceHardware.
+FOUNDATION_EXPORT double UIDeviceHardwareVersionNumber;
+
+//! Project version string for UIDeviceHardware.
+FOUNDATION_EXPORT const unsigned char UIDeviceHardwareVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UIDeviceHardware/PublicHeader.h>
+
+
+#import "UIDeviceHardware.h"
