2333ccb3601b276b6e788344c7d20085719e921b
Hoon H <hh@hpcnt.com>
2017-03-29 04:54:51 +0900

    Add testdrive target.

---
+++ UIDeviceHardwareTestdrive/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  UIDeviceHardwareTestdrive
+//
+//  Created by Hoon Hwangbo on 29/03/2017.
+//  Copyright © 2017 Hyperconnect. All rights reserved.
+//
+
+import UIKit
+import UIDeviceHardware
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
